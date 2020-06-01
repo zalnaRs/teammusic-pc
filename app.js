7ee04bde6b3b1af509bc603e9de025b3e8074096
zalnaRs <thezalnar@hotmail.com>
2020-06-01 13:50:50 +0200

    some chanages

--- app.js
+++ app.js
@@ -9,9 +9,17 @@ var web = ById('web');
 var dirname = app.getAppPath();
 
 function goTo(url, local) {
-        web.loadURL('file://' + dirname + "/" + url);
+    web.loadURL('file://' + dirname + "/" + url);
 }
 
 function load(url) {
     web.loadURL(url);
+}
+
+function snackbar(text) {
+    var snackbarContainer = document.querySelector('#demo-toast-example');
+    var data = {
+        message: text
+    };
+    snackbarContainer.MaterialSnackbar.showSnackbar(data);
 }
